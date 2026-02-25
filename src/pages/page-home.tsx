4091ea86eebf0d93156954fe17394569a86c39b7
GuiDattein <guilhermedattein@gmail.com>
2026-02-25 19:01:22 -0300

    add photo-widget

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,9 +1,25 @@
-import Text from "../components/text";
+import Container from "../components/container";
+import PhotoWidget from "../contexts/photos/components/photo-widget";
+import type { Photo } from "../contexts/photos/models/photo";
 
 export default function PageHome() {
   return (
-    <>
-      <Text className={"text-accent-brand"}>Home</Text>
-    </>
+    <Container>
+      <div className="grid grid-cols-4 gap-9">
+        <PhotoWidget
+          photo={{
+            id: "123",
+            title: "teste",
+            imageId: "portrait-tower.png",
+            albums: [
+              { id: "321", title: "Ablum 1" },
+              { id: "432", title: "Ablum 2" },
+              { id: "643", title: "Ablum 3" },
+            ],
+          }}
+        />
+        <PhotoWidget photo={{} as Photo} loading />
+      </div>
+    </Container>
   );
 }
