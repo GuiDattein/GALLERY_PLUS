9431cbf04f7b4426a1a00b2d065c4193629f7ac2
GuiDattein <guilhermedattein@gmail.com>
2026-03-02 17:30:05 -0300

    add image-preview in page-photo-details

--- src/pages/page-photo-details.tsx
+++ src/pages/page-photo-details.tsx
@@ -4,10 +4,12 @@ import Container from "../components/container";
 import type { Photo } from "../contexts/photos/models/photo";
 import Skeleton from "../components/skeleton";
 import PhotosNavigator from "../contexts/photos/components/photos-navigator";
+import ImageFilePreview from "../components/image-file-preview";
+import Button from "../components/button";
 
 export default function PagePhotoDetails() {
   const { id } = useParams();
-  const isLoadingPhoto = false;
+  const isLoadingPhoto = true;
   const photo = {} as Photo;
 
   return (
@@ -19,8 +21,28 @@ export default function PagePhotoDetails() {
           <Skeleton className="w-48 h-8" />
         )}
 
-        <PhotosNavigator />
+        <PhotosNavigator loading={isLoadingPhoto} />
       </header>
+
+      <div className="grid grid-cols-[21rem] gap-24">
+        <div className="space-y-3">
+          {!isLoadingPhoto ? (
+            <ImageFilePreview
+              src={`/images/${photo?.imageId}`}
+              title={photo?.title}
+              imageClassName="h-[21rem]"
+            />
+          ) : (
+            <Skeleton className="h-[21rem]" />
+          )}
+
+          {!isLoadingPhoto ? (
+            <Button variant={"destructive"}>Excluir</Button>
+          ) : (
+            <Skeleton className="w-20 h-10" />
+          )}
+        </div>
+      </div>
     </Container>
   );
 }
